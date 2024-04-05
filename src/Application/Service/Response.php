b94fe666c9a0888f7dc61ddf350bfea49f4b4da5
Andreas Georgantzelis <andreasgeorgantzelis@gmail.com>
2024-04-05 14:58:35 +0300

    chore : updated response class

--- src/Application/Service/Response.php
+++ src/Application/Service/Response.php
@@ -4,7 +4,21 @@ namespace app\Application\Service;
 
 class Response
 {
+    private array $headers = [];
+
     public function setStatusCode(int $code) {
         http_response_code($code);
     }
+    public function addHeader($name, $value){
+        $this->headers[$name][] = $value;
+    }
+    public function setHeader($name, $value){
+        $this->headers[$name] = [
+            (string) $value,
+        ];
+    }
+    public function redirect($url){
+        $this->setHeader('Location', $url);
+    }
+
 }
